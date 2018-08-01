386e75a0d10cfb5fe862ee89114741f65600103e
karolinneczka <okaa93@gmail.com>
2018-08-01 16:00:35 +0200

    First commit create actions

---
+++ actions.js
@@ -0,0 +1,48 @@
+import uuid from uuid;
+
+/*typy akcji*/
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+/*kreatory akcji*/
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function removeComment() {
+    return {
+        type: REMOVE_COMMENT,
+        id: uuid.v4()
+    }
+}
+
+function editComment(text) {
+    return {
+        type: EDIT_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function thumbUpComment(thumbUp) {
+    return {
+        type: THUMB_UP_COMMENT,
+        thumbUp,
+        id: uuid.v4()
+    }
+}
+
+function addComment(thumbDown) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        thumbDown,
+        id: uuid.v4()
+    }
+}
